54a9f279f555c370900755e1662def7b26572eb3
A <A@192.168.0.12>
2020-12-18 15:42:34 +0100

    remove the gererlots function from chefstock and ouvrier

--- Projet_Java/src/worker/Ouvrier.java
+++ Projet_Java/src/worker/Ouvrier.java
@@ -1,9 +1,6 @@
 package worker;
 
-import material.Lot;
-import operation.GererLots;
-
-public class Ouvrier extends Personne implements GererLots{
+public class Ouvrier extends Personne{
 
 	private String specialite;
 	private final int salaire = 5;
@@ -13,38 +10,5 @@ public class Ouvrier extends Personne implements GererLots{
 		super(identifiant, nom, prenom);
 		this.specialite = specialite;
 	}
-	
-	public Lot deplacer_lot(int lotID, int volume) {
-		return null;		
-	}
-	
-	public Lot ajouter_lot(int lotID, int volume) {
-		return null;
-		
-	}
-	
-	public Lot retirer_lot(int lotID, int volume) {
-		return null;
-		
-	}
-
-	@Override
-	public Lot deplacer_lot(Lot l, int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Lot ajouter_lot(Lot l) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Lot retirer_lot(Lot l) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 
 }
