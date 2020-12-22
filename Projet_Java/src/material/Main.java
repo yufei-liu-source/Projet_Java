1434925652c5eb6e8890da87b928e172cc7324b9
yteka <yteka@192.168.1.3>
2020-12-22 20:35:30 +0100

    added day loop + construire meuble

--- Projet_Java/src/material/Main.java
+++ Projet_Java/src/material/Main.java
@@ -129,7 +129,7 @@ public class Main {
                 	}
                 		}
         			
-        		
+        		//added test
         	
     }
         	
--- Projet_Java/src/operation/GererLots.java
+++ Projet_Java/src/operation/GererLots.java
@@ -1,11 +1,12 @@
 package operation;
 
 import material.Lot;
+import material.Rangee;
 
 public interface GererLots {
 
-	public void deplacer_lot(Lot l, int index);
-	public void ajouter_lot(Lot l);
-	public void retirer_lot(Lot l);
+	public void deplacer_lot(Lot l, Rangee r1, int vol, Rangee r2);
+	public void ajouter_lot(Lot l, int vol, Rangee r);
+	public void retirer_lot(Lot l, int vol, Rangee r);
 
 }
